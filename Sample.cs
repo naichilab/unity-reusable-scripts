f7d741c2b6f9f8bbf8179ff43d962aa2445a538e
naichilab <naichilab@live.jp>
2017-06-09 02:21:54 +0900

    first commit

---
+++ Sample.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace naichilab
+{
+    public class Sample : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            Debug.Log("this is sample script");
+        }
+    }
+}
